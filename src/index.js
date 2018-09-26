b2f7d306b79764e777f5ca17afbaca0fcf970808
VitaliBrych333 <36643236+VitaliBrych333@users.noreply.github.com>
2018-09-27 00:05:06 +0300

    not complete solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function count(s, pairs) {
+ module.exports = function count(s, pairs) {
   // your implementation
+    const answerMod = 1000000007;
+    let k = 1;
+    if (s == "1" || s == "01"){
+		let length = pairs.length;
+        for (let i = 0; i < length; i++) {
+            k *= (pairs[i][0] - 1);
+        }
+        return k % answerMod;
+    }
+    return k % answerMod;
 }
